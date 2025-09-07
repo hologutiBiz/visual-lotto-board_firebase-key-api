d48cd494761a52b1b424dd216a706339a4dda9a8
hologutbiz <holoakinseloyin@gmail.com>
2025-09-07 13:58:27 +0100

    Added Environment variables for BASIC USERS

--- netlify/functions/firebaseConfig.js
+++ netlify/functions/firebaseConfig.js
@@ -3,12 +3,14 @@ function getFirebaseConfig(appName) {
   const appIds = {
     proForecastPanel: process.env.FIREBASE_APP_ID_PRO_FORECAST_PANEL,
     lottoforecastWebApp: process.env.FIREBASE_APP_ID_LOTTO_FORECAST_WEB_APP,
+    basicUserWebApp: process.env.BASIC_USER_WEB_FIREBASE_APP_ID,
     default: process.env.FIREBASE_APP_ID
   };
 
   const measurementIds = {
     proForecastPanel: process.env.FIREBASE_MEASUREMENT_ID_PRO_FORECAST_PANEL,
     lottoforecastWebApp: process.env.FIREBASE_MEASUREMENT_ID_LOTTO_FORECAST_WEB_APP,
+    basicUserWebApp: process.env.BASIC_USER_WEB_FIREBASE_MEASUREMENT_ID,
     default: process.env.FIREBASE_MEASUREMENT_ID
   }
 
